e6b6b6417ae0cb0b2b83a0d9c105dc0045902958
Kramptj KC <kramptj@gmail.com>
2025-10-15 19:11:48 -0400

    Add Python backend and front-end, back-end directories
    - Initialized Python and FastAPI as the backend of the project.
    - Created 2 main directories at the root folder to split between
      front-end and back-end.
    - Created _main.py_ to test FastAPI connection and result was success.
    - Created _requirements.txt_ to store python packages.
    - Verified base API is running successfully.
    - Created a README.md file to add to it later.

---
+++ backEnd/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Signup/Login Backend")
+
+# Allow front end requests 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Root route
+@app.get("/")
+def root():
+    return {"message": "Backend is running successfully!"}
